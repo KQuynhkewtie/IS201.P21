fd1ed95b36eb013301f6beb889a33d78d43bc54e
KQuynhkewtie <dnkquynh2005@gmail.com>
2025-06-08 17:06:35 +0700

    Update dbconnection.java

--- src/DAL/dbconnection.java
+++ src/DAL/dbconnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class dbconnection {
 	public static Connection getConnection() {
 		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
-		String user = "c##DoAn_No3";
-		String pass = "quynh123123";
+		String user = "c##TSISPharmacy";
+		String pass = "tsispharmacy";
 
 		try {
 			Connection conn = DriverManager.getConnection(url, user, pass);
